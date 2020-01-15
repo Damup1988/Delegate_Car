c1d4a0c2c26165464406cd432eb33658398e081b
Damup1988 <damup1988@gmail.com>
2020-01-15 04:38:23 +0300

    Car Delegate. Start version

---
+++ Car.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Car
+    {
+        //Our delegate
+        public delegate void CarStateHandler(string Message);
+
+        //Exemplar of the delegate
+        CarStateHandler mes;
+
+        //Registration of the delegate mes
+        public void RegHandler(CarStateHandler mes)
+        {
+            this.mes += mes;
+        }
+
+        public void UnregHandler(CarStateHandler mes)
+        {
+            this.mes -= mes;
+        }
+
+        public int MaxSpeed { get; set; }
+        public int CurSpeed { get; set; }
+        public string Model { get; set; }
+        public bool CarIsDead { get; set; }
+        public bool CarIsMoving { get; set; }
+
+        public Car()
+        {
+
+        }
+
+        public Car(int MaxSpeed, int CurSpeed, string Model, bool CarIsDead)
+        {
+            this.CurSpeed = CurSpeed;
+            this.MaxSpeed = MaxSpeed;
+            this.Model = Model;
+            this.CarIsDead = CarIsDead;
+            if(this.CurSpeed > 0)
+            {
+                CarIsMoving = true;
+            }
+        }
+
+        public void SpeedUp(int delta)
+        {
+            if (!CarIsDead)
+            {
+                CurSpeed += delta;
+                if (mes != null)
+                    mes($"Current spped is {CurSpeed}");
+                if (MaxSpeed - CurSpeed < 20 & MaxSpeed - CurSpeed > 0)
+                {
+                    if (mes != null)
+                        mes($"Current spped is {CurSpeed}. The car is going to blow!");
+                }
+                if (CurSpeed > MaxSpeed)
+                {
+                    if (mes != null)
+                        mes("The car has blown!");
+                    CarIsDead = true;
+                }
+            }
+            else
+            {
+                if (mes != null)
+                    mes($"{Model} is dead!");
+            }
+        }
+
+        public void SpeedDown (int delta)
+        {
+            if (CarIsMoving)
+            {
+                CurSpeed -= delta;
+                if (mes != null)
+                    mes($"Current spped of {Model} is {CurSpeed}");
+                if (CurSpeed <= 0)
+                {
+                    if (mes != null)
+                        mes($"{Model} has stopped");
+                    CarIsMoving = false;
+                }
+            }
+            else
+            {
+                    mes?.Invoke($"{Model} isn't moving");
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Program
+    {
+        private static void ShowMessage(string mes)
+        {
+            Console.WriteLine(mes);
+        }
+
+        static void Main(string[] args)
+        {
+            Car NewCar = new Car
+            {
+                Model = "BMW",
+                MaxSpeed = 150,
+                CurSpeed = 0,
+                CarIsDead = false
+            };
+
+
+            Car BMW = new Car(300, 150, "BMW Z4", false);
+            NewCar.RegHandler(new Car.CarStateHandler(ShowMessage));
+            BMW.RegHandler(new Car.CarStateHandler(ShowMessage));
+
+            for(int i = 0; i < 10; i++)
+            {
+                NewCar.SpeedUp(20);
+            }
+
+            for (int i = 0; i < 20; i++)
+            {
+                BMW.SpeedDown(10);
+            }
+
+            Console.WriteLine("For Git");
+            Console.ReadLine();
+        }
+    }
+}
